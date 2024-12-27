3e7298c07f2c04a7e70260748d32587b3e23c2ac
SRINITHIk16 <srinithi.k2024aids@sece.ac.in>
2024-12-27 10:29:36 +0530

    Add files via upload

---
+++ v5.js
@@ -0,0 +1,5 @@
+
+document.getElementById('continueButton').addEventListener('click', function() {
+    alert('Continuing to the next step!');
+    // Redirect to another page or perform any action here
+});
